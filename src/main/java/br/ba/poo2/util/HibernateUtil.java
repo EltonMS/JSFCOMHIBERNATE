c3d1299bf07589d29a76f55e2be0ba4a375e47c4
Elton Moreira <elton.santos@portalimap.org.br>
2024-05-14 10:12:17 -0300

    Initial commit

---
+++ src/main/java/br/ba/poo2/util/HibernateUtil.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ba.poo2.util;
+
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    
+    private static SessionFactory buildSessionFactory() {
+        try {
+            AnnotationConfiguration conf = new AnnotationConfiguration();
+            SessionFactory obj = conf.configure("hibernate.cfg.xml").buildSessionFactory();
+            return obj;
+            } catch (Throwable ex) {
+            System.err.println("Falha ao Iniciar Sessão." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
